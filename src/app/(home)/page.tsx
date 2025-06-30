54224660f6bd5b4729803196ca84569af29826b6
mehedi8430 <mehedialhasan1999@gmail.com>
2025-06-30 06:48:56 +0600

    refactor: Remove unused HeaderSection import from Home component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,3 @@
-import HeaderSection from "@/components/home/headerSection";
 import HeroSection from "@/components/home/heroSection";
 import OurApproach from "@/components/home/OurApproach";
 import SupportHighlights from "@/components/home/SupportHighlights";
@@ -7,7 +6,6 @@ import Testimonials from "@/components/home/testimonial";
 export default function Home() {
   return (
     <>
-      <HeaderSection />
       <HeroSection />
       <Testimonials />
       <OurApproach />
