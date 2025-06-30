03938f7431fe7ca46e46fede3971555815ff6d5d
fiamanillah <fiamanillahb2@gmail.com>
2025-07-01 00:05:06 +0600

    Add new service section with icons and verification logos
    - Introduced a new services section in the home component, displaying various accounting services offered.
    - Added SVG icons for check marks and verification logos in different colors (blue, green, yellow).
    - Each service includes a title, description items, and an associated image.
    - Implemented a responsive layout for better presentation of services.
    - Created a reusable Separator component for visual separation between services.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,17 @@
-import HeroSection from "@/components/home/heroSection";
-import OurApproach from "@/components/home/OurApproach";
-import SupportHighlights from "@/components/home/SupportHighlights";
-import Testimonials from "@/components/home/testimonial";
+import HeroSection from '@/components/home/heroSection';
+import OurApproach from '@/components/home/OurApproach';
+import ServicesSection from '@/components/home/servicesSection';
+import SupportHighlights from '@/components/home/SupportHighlights';
+import Testimonials from '@/components/home/testimonial';
 
 export default function Home() {
-  return (
-    <>
-      <HeroSection />
-      <Testimonials />
-      <OurApproach />
-      <SupportHighlights />
-    </>
-  );
+    return (
+        <>
+            <HeroSection />
+            <Testimonials />
+            <OurApproach />
+            <SupportHighlights />
+            <ServicesSection />
+        </>
+    );
 }
