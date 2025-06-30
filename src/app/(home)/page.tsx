cb3e2e4d60ad512164d223597cf2c7ce23ab08cd
mehedi8430 <mehedialhasan1999@gmail.com>
2025-06-30 06:44:36 +0600

    Add WhatsApp SVG icon to assets

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,8 @@
-import HeaderSection from "@/components/home/headerSection";
 import HeroSection from "@/components/home/heroSection";
 
 export default function Home() {
   return (
     <>
-      <HeaderSection />
       <HeroSection />
     </>
   );
