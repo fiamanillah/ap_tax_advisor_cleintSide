c61ff5c93870d783165abb2cd1ac6d0ee1e02884
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-07-03 00:53:02 +0600

    feat: add OurValues component with accordion display for values section

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,12 +1,15 @@
 import OurMission from "@/components/about-us/OurMission";
+import OurValues from "@/components/about-us/OurValues";
 import Hero from "@/components/Shared/Hero";
 import { heroSectionProps } from "./heroData";
+import { ourValueSectionProps } from "./ourValueData";
 
 export default function page() {
   return (
     <main>
       <Hero {...heroSectionProps} />
       <OurMission />
+      <OurValues {...ourValueSectionProps} />
     </main>
   );
 }
