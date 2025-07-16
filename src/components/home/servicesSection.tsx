4c2fe897b1b75e43ced00a9084d33f4d9925b8b3
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-07-17 03:53:54 +0600

    feat: Update service items and enhance layout in ServicesSection for improved clarity and user experience

--- src/components/home/servicesSection.tsx
+++ src/components/home/servicesSection.tsx
@@ -7,69 +7,73 @@ import { Card } from "../ui/card";
 export default function ServicesSection({ data }: TService) {
   return (
     <section>
-      <div className="container lg:max-w-6xl mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-center mb-8">
-          What&apos;s Included
-        </h1>
-        <div className="flex flex-col gap-20">
-          {data.map((service, index) => (
-            <div
-              key={index}
-              className={cn("flex flex-col w-full", {
-                "items-start": service.align === "left",
-                "items-end": service.align === "right",
-              })}>
-              {service.items && (
-                <div
-                  className={cn(
-                    "w-full flex max-lg:flex-col items-center gap-8 justify-between",
-                    {
-                      "md:flex-row-reverse": service.align === "right",
-                    }
-                  )}>
-                  <div>
-                    <h1 className="text-2xl mb-5 font-medium">
-                      {service.title}
-                    </h1>
-                    <ul className="space-y-2">
-                      {service.items.map((item, idx) => (
-                        <li key={idx} className="flex items-start">
-                          <Image
-                            src="/icons/check.svg"
-                            alt="Check"
-                            width={24}
-                            height={24}
-                          />
-                          <span>{item}</span>
-                        </li>
-                      ))}
-                    </ul>
+      <div className="container mx-auto px-4 py-8">
+        <div className="section-inverted-radius-reverse">
+          <h1 className="text-background mb-8 text-center text-[40px] font-medium">
+            What&apos;s Included
+          </h1>
+          <div className="flex flex-col gap-20">
+            {data.map((service, index) => (
+              <div
+                key={index}
+                className={cn("flex w-full flex-col", {
+                  "items-start": service.align === "left",
+                  "items-end": service.align === "right",
+                })}
+              >
+                {service.items && (
+                  <div
+                    className={cn(
+                      "flex w-full items-center justify-between gap-8 max-lg:flex-col",
+                      {
+                        "md:flex-row-reverse": service.align === "right",
+                      },
+                    )}
+                  >
+                    <div>
+                      <h1 className="text-background mb-5 text-2xl font-medium">
+                        {service.title}
+                      </h1>
+                      <ul className="text-muted-foreground max-w-xl space-y-2">
+                        {service.items.map((item, idx) => (
+                          <li key={idx} className="flex items-start gap-2">
+                            <Image
+                              src="/icons/check.svg"
+                              alt="Check"
+                              width={24}
+                              height={24}
+                            />
+                            <span>{item}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+
+                    <Card className="from-secondary-gradient to-primary-gradient relative flex w-full flex-col items-center border-0 bg-gradient-to-bl p-4 shadow-xl sm:w-fit">
+                      <Image
+                        src={service.image}
+                        alt="Separator"
+                        width={150}
+                        height={150}
+                      />
+                      <h1 className="text-muted-foreground max-w-[250px] text-center text-xl font-medium">
+                        {service.imageTitle}
+                      </h1>
+                      <Image
+                        src={service.verifyLogo}
+                        alt="Separator"
+                        width={50}
+                        height={50}
+                        className="absolute -top-5 -right-5"
+                      />
+                    </Card>
                   </div>
+                )}
 
-                  <Card className="p-4 flex flex-col items-center relative w-full sm:w-fit">
-                    <Image
-                      src={service.image}
-                      alt="Separator"
-                      width={150}
-                      height={150}
-                    />
-                    <h1 className="text-center text-2xl font-medium w-1/2">
-                      {service.imageTitle}
-                    </h1>
-                    <Image
-                      src={service.verifyLogo}
-                      alt="Separator"
-                      width={50}
-                      height={50}
-                      className="absolute -top-5 -right-5"
-                    />
-                  </Card>
-                </div>
-              )}
-
-              {/* {index < services.length - 2 && <Separator className="my-8" />} */}
-            </div>
-          ))}
+                {/* {index < services.length - 2 && <Separator className="my-8" />} */}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
