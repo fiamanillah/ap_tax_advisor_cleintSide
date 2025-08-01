5aa4b1cc21a6ed71c0ee5f0f529a970ab895870c
Rashad-Stack <tex.eng.rashaed@gmail.com>
2025-08-01 22:57:16 +0600

    Add SVG icons for Apple, Back, and Google; implement custom login hook with validation

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,12 +1,21 @@
+import Image from "next/image";
 import React from "react";
 
 export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main>
-      <div className="">
-        <h1>Authentication</h1>
+    <main className="bg-background flex h-screen w-full flex-col items-center justify-center">
+      <div className="container mx-auto flex h-full p-4">
+        <div className="h-full w-7/12">
+          <Image
+            src="/assets/auth.png"
+            alt="Authentication Illustration"
+            width={1280}
+            height={720}
+            className="mb-4 h-full w-full rounded-l-4xl object-fill"
+          />
+        </div>
         {children}
       </div>
     </main>
