4188872d2d4836f7ac40dec87a79a857604fd624
Rashad-Stack <tex.eng.rashaed@gmail.com>
2025-08-01 23:15:39 +0600

    feat(auth): implement registration form with validation and submission handling

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,172 @@
+"use client";
+
+import { AppButton } from "@/components/Shared/AppButton";
+import Icon from "@/components/Shared/Icon";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import useRegister from "@/hooks/use-register";
+import Link from "next/link";
+
+export default function Register() {
+  const { form, onSubmit } = useRegister();
+  return (
+    <div className="gradient-90 relative right-10 flex h-full w-6/12 items-center justify-center rounded-4xl">
+      <Link href="/auth/register">
+        <Button className="absolute top-8 left-8" variant="ghost">
+          <Icon src="/icons/back.svg" />
+        </Button>
+      </Link>
+      <div className="w-full max-w-xl">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-8 p-8"
+          >
+            <div className="flex gap-4">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-muted-foreground">
+                      First name*
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        className="text-muted-foreground placeholder:text-muted-foreground rounded-sm border !bg-white"
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel className="text-muted-foreground">
+                      Last name*
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        className="text-muted-foreground placeholder:text-muted-foreground !bg-white"
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel className="text-muted-foreground">
+                    Email address*
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      className="text-muted-foreground placeholder:text-muted-foreground !bg-white"
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel className="text-muted-foreground">
+                    Password*
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      className="text-muted-foreground placeholder:text-muted-foreground !bg-white"
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel className="text-muted-foreground">
+                    Confirm password*
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      className="text-muted-foreground placeholder:text-muted-foreground !bg-white"
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <AppButton type="submit" className="w-full">
+              Submit
+            </AppButton>
+            <div className="flex justify-end">
+              <Link href="/auth/forgot-password">
+                <span className="text-muted-foreground hover:text-primary text-sm">
+                  Forgot password?
+                </span>
+              </Link>
+            </div>
+          </form>
+        </Form>
+        <div className="mb-5 flex items-center gap-x-10 px-10">
+          <span className="bg-muted-foreground/30 block h-0.5 flex-1"></span>
+          <span className="text-muted-foreground">or Log in with</span>
+          <span className="bg-muted-foreground/30 block h-0.5 flex-1"></span>
+        </div>
+        <div className="flex items-center justify-center gap-10 px-16">
+          <Button
+            variant="outline"
+            className="text-muted-foreground !border-muted-foreground hover:text-muted-foreground flex-1 cursor-pointer rounded-2xl"
+            size="lg"
+          >
+            <Icon src="/icons/apple.svg" /> <span>Apple ID</span>
+          </Button>
+
+          <Button
+            variant="outline"
+            className="text-muted-foreground !border-muted-foreground hover:text-muted-foreground flex-1 cursor-pointer rounded-2xl"
+            size="lg"
+          >
+            <Icon src="/icons/google.svg" /> <span>Google</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/hooks/use-register.ts
@@ -0,0 +1,43 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  firstName: z.string().min(1, {
+    message: "First name is required.",
+  }),
+  lastName: z.string().min(1, {
+    message: "Last name is required.",
+  }),
+  email: z.string().email({
+    message: "Invalid email address.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+  confirmPassword: z.string().min(6, {
+    message: "Confirm password must be at least 6 characters.",
+  }),
+});
+
+export default function useRegister() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return { form, onSubmit };
+}
